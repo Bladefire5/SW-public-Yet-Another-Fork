a4ec13c2c9dbe45792c5b640f6cbd48f3d59b2be
Maxwell1007 <maxsbel@gmail.com>
2026-08-23 16:22:21 +0300

    Кровавая весшунка теперь еда для ноктюрнов

---
+++ Content.Server/Imperial/Medieval/Nocturn/NocturnBloodFoodComponent.cs
@@ -0,0 +1,14 @@
+namespace Content.Server.Nocturn;
+
+[RegisterComponent]
+public sealed partial class NocturnBloodFoodComponent : Component
+{
+    [DataField]
+    public float BloodRestore = 15f;
+
+    [DataField]
+    public float BloodLevelCap = 200f;
+
+    [DataField]
+    public float EatDuration = 1f;
+}
